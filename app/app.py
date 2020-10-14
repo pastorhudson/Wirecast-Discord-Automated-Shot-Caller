4aae1503579752c656147b75478652b06ebb0662
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2020-10-14 12:23:57 -0400

    Added 404 catch

--- app/app.py
+++ app/app.py
@@ -1,13 +1,19 @@
 from flask import Flask, jsonify
-from flask import request
+from flask import request, redirect
 app = Flask(__name__)
 
 shot_status = {"live": "", "queued": ""}
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return redirect('/')
+
+
 @app.route('/')
 def hello_world():
-    return 'This is a cbc-media tool. Not much to see here.'
+    return '<h1>This is a cbc-media tool. Not much to see here.</h1>'
 
 
 @app.route('/api/shot_status/', methods=['GET', 'POST'])
